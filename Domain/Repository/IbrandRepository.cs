da7dceb55f0cde3f7fb930a49bf7d49036185503
MagicExist <johhanparra1@gmail.com>
2024-09-22 14:53:09 -0500

    IbeerRepository methods added

---
+++ Domain/Repository/IbrandRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repository
+{
+    public interface IbrandRepository
+    {
+        //Get all brands
+        //Add a new brand
+        //Update an existing brand
+        //Delete one brand
+    }
+}
